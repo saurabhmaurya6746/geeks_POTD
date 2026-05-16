4e380a2b60272b7a1a16eb61fa6219a2a8f1ed29
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-05-16 19:38:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Not a subset sum/not-a-subset-sum.py
+++ Difficulty: Medium/Not a subset sum/not-a-subset-sum.py
@@ -1,33 +1,13 @@
-#User function Template for python3
-
 class Solution:
-     
     def findSmallest(self, arr):
-        ans = 1
-        for i in range(len(arr)):
-            if arr[i] > ans:
-                return ans
-            ans += arr[i]
-        return ans
-
-        # code here
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-
-def main():
-    t = int(input())
-    for _ in range(t):
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        ans = ob.findSmallest(arr)
-        print(ans)
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+        arr.sort()
+        
+        res = 1   # smallest value we are trying to form
+        
+        for num in arr:
+            if num > res:
+                return res
+            
+            res += num
+        
+        return res
